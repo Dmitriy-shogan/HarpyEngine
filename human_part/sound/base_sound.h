90e09d3ff6dad3546e27ce67fdc773c231df32dc
дмитрий антипов <limbo.crow@mail.ru>
2023-04-01 01:25:52 +0300

    Added audio decoders library

--- human_part/sound/base_sound.h
+++ human_part/sound/base_sound.h
@@ -2,18 +2,30 @@
 
 #ifndef HARPY_SOUND
 #define HARPY_SOUND
+#pragma comment(lib, "avcodec.lib")
 #include <nest/utilities/harpy_little_error.h>
+#include <libavcodec/avcodec.h>
 #include <AL/efx.h>
 
 namespace harpy_sound{
 class base_sound
 {
-    //TODO: ogg, mp3, wav readers AT LEAST, then proceed to actually work with sound
-    
+    std::unique_ptr<char> data{std::make_unique<char>()};
+
 public:
-    base_sound()
+    base_sound(std::string way_to_file)
     {
+        read(way_to_file);
         
+    }
+
+    void read(std::string way_to_file)
+    {
+        std::ifstream reader(way_to_file);
+        reader.seekg(0, std::ios::end);
+        int file_size = reader.tellg();
+        reader.seekg(0, std::ios::beg);
+        reader.read(data.get(), file_size);
     }
     
 };
